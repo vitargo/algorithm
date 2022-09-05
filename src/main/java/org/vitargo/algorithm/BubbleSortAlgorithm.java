0234f4db3ae04db06e35ee92178d5bbcee557699
viktoriia.redko <viktoriia.redko@ericsson.com>
2022-09-06 00:30:18 +0300

    Add Bubble Sort algorithm

---
+++ src/main/java/org/vitargo/algorithm/BubbleSortAlgorithm.java
@@ -0,0 +1,19 @@
+package org.vitargo.algorithm;
+
+public class BubbleSortAlgorithm {
+
+    public static void iteratively(int[] array) {
+        int temp = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    //TODO: Add optimized algorithm
+}
